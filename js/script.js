5d8157d00d9d84ef4187d7ddbd87e413a2d67fb7
JeerTeles <jadersoncontatos@gmail.com>
2023-09-30 23:41:23 -0300

    Finalizado

---
+++ js/script.js
@@ -0,0 +1,22 @@
+const body = document.body;
+const endTime = new Date('Decembre 31 2023 23:59:59');
+const daysE1 = document.getElementById('days');
+const hoursE1 = document.getElementById('hours');
+const minutesE1 = document.getElementById ('minutes');
+const secondsE1 = document.getElementById('seconds');
+
+setInterval(updateCountdown, 1000);
+
+function updateCountdown() {
+    const startTime = new Date();
+    const diff = endTime - startTime; 
+    const days = Math.floor(diff / 1000 / 60 / 60 / 24);
+    const hours = Math.floor(diff / 1000 / 60 / 60) % 24;
+    const minutes = Math.floor(diff / 1000 / 60) % 60;
+    const seconds = Math.floor(diff / 1000) % 60;
+    daysE1.innerHTML = days;
+    hoursE1.innerHTML = hours < 10 ? '0'+hours : hours;
+    minutesE1.innerHTML = minutes < 10 ? '0'+minutes : minutes;
+    secondsE1.innerHTML = seconds , 10 ? '0'+seconds : seconds;
+}
+
