ff22127f436cc0818969c7a899082f4171e63ff1
JeerTeles <jadersoncontatos@gmail.com>
2023-12-31 12:29:07 -0300

    finalizando

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 const body = document.body;
-const endTime = new Date('Decembre 31 2023 23:59:59');
+const endTime = new Date('Decembre 31 2023 12:26:00');
 const daysE1 = document.getElementById('days');
 const hoursE1 = document.getElementById('hours');
 const minutesE1 = document.getElementById ('minutes');
@@ -18,5 +18,15 @@ function updateCountdown() {
     hoursE1.innerHTML = hours < 10 ? '0'+hours : hours;
     minutesE1.innerHTML = minutes < 10 ? '0'+minutes : minutes;
     secondsE1.innerHTML = seconds , 10 ? '0'+seconds : seconds;
+
+    if (diff <= 0) {
+       showModal();
+    }
+}
+
+
+function showModal() {
+    const modal = document.getElementById('myModal');
+    modal.style.display = 'block';
 }
 
